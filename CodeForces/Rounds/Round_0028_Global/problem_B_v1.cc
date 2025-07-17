a56d7881e8b6d2c0c1d11e17abe4f441ce828fd1
Costantino <xtremexspc@gmail.com>
2025-07-18 00:03:45 +0200

    refactor: Refactor Codeforces Round 28 Global: "XtremeXSPC" solutions.

---
+++ CodeForces/Rounds/Round_0028_Global/problem_B_v1.cc
@@ -0,0 +1,66 @@
+//===---------------------------------------------------------------------===//
+/**
+ * @brief Codeforces Global Round 28
+ * @author: Costantino Lombardi
+ * @file: problem_B_v1.cc
+ *
+ * @status: NOT PASSED
+ */
+//===---------------------------------------------------------------------===//
+/* Included library */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+//===---------------------------------------------------------------------===//
+/* Main function */
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, k;
+    cin >> n >> k;
+
+    vector<pair<int, int>> coverageIndex(n);
+    for (int i = 1; i <= n; i++) {
+      int c;
+      if (i <= k)
+        c = i;
+      else if (i <= n - k + 1)
+        c = k;
+      else
+        c = n - i + 1;
+      coverageIndex[i - 1] = {c, i};
+    }
+
+    sort(coverageIndex.begin(), coverageIndex.end(), [](auto& a, auto& b) {
+      if (a.first != b.first)
+        return a.first > b.first;
+      return a.second < b.second;
+    });
+
+    vector<int> p(n, 0);
+    for (int i = 0; i < n; i++) {
+      p[coverageIndex[i].second - 1] = i + 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+      cout << p[i] << (i + 1 == n ? '\n' : ' ');
+    }
+  }
+
+  return 0;
+}
+
+//===---------------------------------------------------------------------===//
---
+++ CodeForces/Rounds/Round_0028_Global/problem_H.cc
@@ -0,0 +1,100 @@
+//===---------------------------------------------------------------------===//
+/**
+ * @brief Codeforces Global Round 28
+ * @author: Costantino Lombardi
+ * @file: problem_H.cc
+ *
+ * @status: NOT PASSED
+ */
+//===---------------------------------------------------------------------===//
+/* Included library */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+//===---------------------------------------------------------------------===//
+/* Main function */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 998244353;
+
+long long modexp(long long base, long long exp) {
+  long long result = 1 % MOD;
+  long long cur    = base % MOD;
+  while (exp > 0) {
+    if (exp & 1)
+      result = (result * cur) % MOD;
+    cur = (cur * cur) % MOD;
+    exp >>= 1;
+  }
+  return result;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+    int n = (int)s.size();
+    int c = 0; // conta '1'
+    for (auto ch : s)
+      if (ch == '1')
+        c++;
+
+    if (c == 0) {
+      // tutti zero
+      cout << n % MOD << "\n";
+      continue;
+    }
+
+    // trova l
+    int first_one = 0;
+    while (first_one < n && s[first_one] != '1')
+      first_one++;
+    int l = first_one;
+
+    // trova r
+    int last_one = n - 1;
+    while (last_one >= 0 && s[last_one] != '1')
+      last_one--;
+    int r = (n - 1 - last_one);
+
+    // blocchi di zeri tra '1'
+    vector<int> zero_blocks;
+    int         prev_one = first_one;
+    for (int i = first_one + 1; i <= last_one; i++) {
+      if (s[i] == '1') {
+        int zeros_count = i - prev_one - 1;
+        if (zeros_count > 0)
+          zero_blocks.push_back(zeros_count);
+        prev_one = i;
+      }
+    }
+
+    long long ans = 1;
+    ans           = ans * (l + 1) % MOD;
+    ans           = ans * (r + 1) % MOD;
+    for (auto z : zero_blocks)
+      ans = ans * (z + 1) % MOD;
+    ans = ans * modexp(2, c - 1) % MOD;
+
+    cout << ans << "\n";
+  }
+
+  return 0;
+}
+
+//===---------------------------------------------------------------------===//
