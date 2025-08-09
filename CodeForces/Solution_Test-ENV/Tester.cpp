880deba9993ca6ea61dbe67f69854d710e3b41bf
LCS-Dev.Mac <xtremexspc@gmail.com>
2025-08-09 13:29:21 +0200

    chore: Update project configuration and include necessary headers for pb_ds compatibility.

--- CodeForces/Solution_Test-ENV/Tester.cpp
+++ CodeForces/Solution_Test-ENV/Tester.cpp
@@ -11,6 +11,8 @@
 //===---------------------------------------------------------------------===//
 /* Included library */
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #include <iostream>
 
 //===---------------------------------------------------------------------===//
