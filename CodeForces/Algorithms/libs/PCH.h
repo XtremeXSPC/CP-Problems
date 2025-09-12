bd9d672718d4cdc619efa3fb05c3845ff37aa689
LCS-Dev.Mac <xtremexspc@gmail.com>
2025-09-12 13:17:24 +0200

    Refactor type aliases and container utilities for clarity and consistency across headers.

--- CodeForces/Algorithms/libs/PCH.h
+++ CodeForces/Algorithms/libs/PCH.h
@@ -460,7 +460,8 @@
 //================== ENHANCED COMPETITIVE PROGRAMMING TYPES ==================//
 
 #ifndef __TYPES__
-  // Type aliases with explicit semantics.
+#define __TYPES__
+  // Fundamental type aliases with explicit sizes:
   using I8  = std::int8_t;
   using I16 = std::int16_t;
   using I32 = std::int32_t;
@@ -470,79 +471,120 @@
   using U32 = std::uint32_t;
   using U64 = std::uint64_t;
 
-  // Extended precision types.
+  // Extended precision types (when available):
   #ifdef __SIZEOF_INT128__
-      using I128 = __int128;
-      using U128 = unsigned __int128;
-      #define HAS_INT128 1
+    using I128 = __int128;
+    using U128 = unsigned __int128;
+    #define HAS_INT128 1
   #else
-      using I128 = I64;
-      using U128 = U64;
-      #define HAS_INT128 0
+    // Fallback for compilers that don't support 128-bit integers.
+    using I128 = std::int64_t;
+    using U128 = std::uint64_t;
+    #define HAS_INT128 0
   #endif
 
-  // Floating point types.
+  // Floating point types:
   using F32 = float;
   using F64 = double;
   using F80 = long double;
 
   #ifdef __FLOAT128__
-      using F128 = __float128;
-      #define HAS_FLOAT128 1
+    using F128 = __float128;
+    #define HAS_FLOAT128 1
   #else
-      using F128 = F80;
-      #define HAS_FLOAT128 0
+    // Fallback for compilers that don't support 128-bit floats.
+    using F128 = long double;
+    #define HAS_FLOAT128 0
   #endif
 
-  // Legacy compatibility aliases.
+  // Legacy aliases for backward compatibility:
   using ll  = I64;
   using ull = U64;
   using ld  = F80;
 
-  // Container type aliases.
-  template<class T> using Vec = std::vector<T>;
-  template<class T> using Deque = std::deque<T>;
-  template<class T> using List = std::list<T>;
-  template<class T> using Set = std::set<T>;
-  template<class T> using MultiSet = std::multiset<T>;
-  template<class T> using UnorderedSet = std::unordered_set<T>;
-  template<class K, class V> using Map = std::map<K, V>;
-  template<class K, class V> using MultiMap = std::multimap<K, V>;
-  template<class K, class V> using UnorderedMap = std::unordered_map<K, V>;
-  template<class T> using Stack = std::stack<T, std::deque<T>>;
-  template<class T> using Queue = std::queue<T, std::deque<T>>;
-  template<class T> using PriorityQueue = std::priority_queue<T, std::vector<T>>;
-  template<class T> using MinPriorityQueue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+  // Container type aliases:
+  template <class T>
+  using Vec = std::vector<T>;
+  template <class T>
+  using Deque = std::deque<T>;
+  template <class T>
+  using List = std::list<T>;
+  template <class T>
+  using Set = std::set<T>;
+  template <class T>
+  using MultiSet = std::multiset<T>;
+  template <class T>
+  using UnorderedSet = std::unordered_set<T>;
+  template <class K, class V>
+  using Map = std::map<K, V>;
+  template <class K, class V>
+  using MultiMap = std::multimap<K, V>;
+  template <class K, class V>
+  using UnorderedMap = std::unordered_map<K, V>;
+  template <class T>
+  using Stack = std::stack<T, std::deque<T>>;
+  template <class T>
+  using Queue = std::queue<T, std::deque<T>>;
+  template <class T>
+  using PriorityQueue = std::priority_queue<T, std::vector<T>>;
+  template <class T>
+  using MinPriorityQueue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
 
-  // Pair and tuple aliases.
-  template<class T, class U> using Pair = std::pair<T, U>;
+  // Short aliases for competitive programming:
+  template <class T>
+  using VC = Vec<T>;
+  template <class T>
+  using VVC = VC<VC<T>>;
+  template <class T>
+  using VVVC = VC<VVC<T>>;
+
+  // Pair and tuple aliases:
+  template <class T, class U>
+  using Pair = std::pair<T, U>;
+  template <class T, class U>
+  using P = Pair<T, U>;
+
   using PII = Pair<I32, I32>;
   using PLL = Pair<I64, I64>;
   using PLD = Pair<F80, F80>;
 
-  // Common vector types.
-  using VI  = Vec<I32>;
-  using VLL = Vec<I64>;
-  using VU8 = Vec<U8>;
-  using VF  = Vec<F64>;
-  using VS  = Vec<std::string>;
-  using VB  = Vec<bool>;
-
-  // 2D containers.
-  using VVI = Vec<VI>;
+  // Specialized container aliases:
+  using VI   = Vec<I32>;
+  using VLL  = Vec<I64>;
+  using VVI  = Vec<VI>;
   using VVLL = Vec<VLL>;
-  using VVVF = Vec<VF>;
+  using VB   = Vec<bool>;
+  using VS   = Vec<std::string>;
+  using VU8  = Vec<U8>;
+  using VU32 = Vec<U32>;
+  using VU64 = Vec<U64>;
+  using VF   = Vec<F64>;
 
-  // Vector of pairs.
+  // Vector of pairs:
   using VPII = Vec<PII>;
   using VPLL = Vec<PLL>;
-  #define __TYPES__
+  template <class T, class U>
+  using VP = Vec<P<T, U>>;
+
+  // Policy-based data structures:
+  #ifdef PBDS_AVAILABLE
+    using namespace __gnu_pbds;
+    template <typename T>
+    using ordered_set = tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+    template <typename T>
+    using ordered_multiset = tree<T, null_type, std::less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+    template <typename K, typename V>
+    using ordered_map = tree<K, V, std::less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+    template <typename K, typename V>
+    using gp_hash_table = __gnu_pbds::gp_hash_table<K, V, std::hash<K>, std::equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>, hash_standard_resize_policy<hash_exponential_size_policy<>, hash_load_check_resize_trigger<>, true>>;
+  #endif
 #endif
   
 //===----------------------------------------------------------------------===//
 //========================== MATHEMATICAL CONSTANTS ==========================//
 
 #ifndef __CONSTANTS__
+#define __CONSTANTS__
   // High precision mathematical constants.
   #ifdef HAS_NUMBERS
     using std::numbers::pi_v;
@@ -599,13 +641,13 @@
     constexpr I64 MOD  = 1000000007;
     constexpr I64 MOD2 = 998244353;
     constexpr I64 MOD3 = 1000000009;
-  #define __CONSTANTS__
 #endif
   
 //===----------------------------------------------------------------------===//
 //================== ENHANCED UTILITY FUNCTIONS AND MACROS ===================//
 
 #ifndef __UTILITY_FUNCTIONS__
+#define __UTILITY_FUNCTIONS__
   // Bitwise operations with modern implementations.
   template<std::integral T>
   ALWAYS_INLINE constexpr I32 popcount(T x) noexcept {
@@ -733,7 +775,6 @@
   ALWAYS_INLINE constexpr bool chmax(T& a, const S& b) noexcept {
       return (a < b) ? (a = b, true) : false;
   }
-#define __UTILITY_FUNCTIONS__
 #endif
   
 //===----------------------------------------------------------------------===//
--- CodeForces/Algorithms/templates/Types.hpp
+++ CodeForces/Algorithms/templates/Types.hpp
@@ -3,6 +3,7 @@
 
 //===----------------------------------------------------------------------===//
 /* Advanced Type System and Aliases */
+// clang-format off
 
 // Fundamental type aliases with explicit sizes:
 using I8  = std::int8_t;
@@ -13,25 +14,31 @@ using U8  = std::uint8_t;
 using U16 = std::uint16_t;
 using U32 = std::uint32_t;
 using U64 = std::uint64_t;
+
+// Extended precision types (when available):
+#ifdef __SIZEOF_INT128__
+  using I128 = __int128;
+  using U128 = unsigned __int128;
+#define HAS_INT128 1
+#else
+  // Fallback for compilers that don't support 128-bit integers.
+  using I128 = std::int64_t;
+  using U128 = std::uint64_t;
+#define HAS_INT128 0
+#endif
+
+// Floating point types:
 using F32 = float;
 using F64 = double;
 using F80 = long double;
 
-// Extended precision types (when available):
-#ifdef __SIZEOF_INT128__
-using I128 = __int128;
-using U128 = unsigned __int128;
-#else
-// Fallback for compilers that don't support 128-bit integers
-using I128 = std::int64_t;
-using U128 = std::uint64_t;
-#endif
-
 #ifdef __FLOAT128__
-using F128 = __float128;
+  using F128 = __float128;
+#define HAS_FLOAT128 1
 #else
-// Fallback for compilers that don't support 128-bit floats
-using F128 = long double;
+  // Fallback for compilers that don't support 128-bit floats.
+  using F128 = long double;
+#define HAS_FLOAT128 0
 #endif
 
 // Legacy aliases for backward compatibility:
@@ -39,55 +46,79 @@ using ll  = I64;
 using ull = U64;
 using ld  = F80;
 
-// Container type aliases with template parameters:
+// Container type aliases:
 template <class T>
-using VC = std::vector<T>;
+using Vec = std::vector<T>;
+template <class T>
+using Deque = std::deque<T>;
+template <class T>
+using List = std::list<T>;
+template <class T>
+using Set = std::set<T>;
+template <class T>
+using MultiSet = std::multiset<T>;
+template <class T>
+using UnorderedSet = std::unordered_set<T>;
+template <class K, class V>
+using Map = std::map<K, V>;
+template <class K, class V>
+using MultiMap = std::multimap<K, V>;
+template <class K, class V>
+using UnorderedMap = std::unordered_map<K, V>;
+template <class T>
+using Stack = std::stack<T, std::deque<T>>;
+template <class T>
+using Queue = std::queue<T, std::deque<T>>;
+template <class T>
+using PriorityQueue = std::priority_queue<T, std::vector<T>>;
+template <class T>
+using MinPriorityQueue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+// Short aliases for competitive programming:
+template <class T>
+using VC = Vec<T>;
 template <class T>
 using VVC = VC<VC<T>>;
 template <class T>
 using VVVC = VC<VVC<T>>;
-template <class T>
-using VVVVC = VC<VVVC<T>>;
-
-// Specialized container aliases:
-using VI   = VC<I64>;
-using VVI  = VVC<I64>;
-using VVVI = VVVC<I64>;
-using VB   = VC<bool>;
-using VS   = VC<std::string>;
-using VU8  = VC<U8>;
-using VU32 = VC<U32>;
-using VU64 = VC<U64>;
 
 // Pair and tuple aliases:
-using PII = std::pair<I32, I32>;
-using PLL = std::pair<I64, I64>;
-using PLD = std::pair<ld, ld>;
 template <class T, class U>
-using P = std::pair<T, U>;
-
-using VPII = VC<PII>;
-using VPLL = VC<PLL>;
+using Pair = std::pair<T, U>;
 template <class T, class U>
-using VP = VC<P<T, U>>;
+using P = Pair<T, U>;
 
-// Priority queue aliases:
-template <class T>
-using PQ_max = std::priority_queue<T>;
-template <class T>
-using PQ_min = std::priority_queue<T, VC<T>, std::greater<T>>;
+using PII = Pair<I32, I32>;
+using PLL = Pair<I64, I64>;
+using PLD = Pair<F80, F80>;
 
-// Advanced container aliases:
-template <class K, class V>
-using UMap = std::unordered_map<K, V>;
-template <class T>
-using USet = std::unordered_set<T>;
-template <class T>
-using MSet = std::multiset<T>;
+// Specialized container aliases:
+using VI   = Vec<I32>;
+using VLL  = Vec<I64>;
+using VVI  = Vec<VI>;
+using VVLL = Vec<VLL>;
+using VB   = Vec<bool>;
+using VS   = Vec<std::string>;
+using VU8  = Vec<U8>;
+using VU32 = Vec<U32>;
+using VU64 = Vec<U64>;
+using VF   = Vec<F64>;
+
+// Vector of pairs:
+using VPII = Vec<PII>;
+using VPLL = Vec<PLL>;
+template <class T, class U>
+using VP = Vec<P<T, U>>;
 
 // Policy-based data structures:
-using namespace __gnu_pbds;
-template <typename T>
-using ordered_set = tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template <typename T>
-using ordered_multiset = tree<T, null_type, std::less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#ifdef PBDS_AVAILABLE
+  using namespace __gnu_pbds;
+  template <typename T>
+  using ordered_set = tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+  template <typename T>
+  using ordered_multiset = tree<T, null_type, std::less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+  template <typename K, typename V>
+  using ordered_map = tree<K, V, std::less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+  template <typename K, typename V>
+  using gp_hash_table = __gnu_pbds::gp_hash_table<K, V, std::hash<K>, std::equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>, hash_standard_resize_policy<hash_exponential_size_policy<>, hash_load_check_resize_trigger<>, true>>;
+#endif
