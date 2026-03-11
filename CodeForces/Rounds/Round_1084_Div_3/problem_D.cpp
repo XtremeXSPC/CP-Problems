f678e3035c7e2a1b0ff54ef7212217fceb197156
LCS-Dev.Mac <xtremexspc@gmail.com>
2026-03-11 23:43:05 +0100

    Add problem D solution and input/output files for Codeforces Round 1084 (Div. 3).

--- CodeForces/Rounds/Round_1084_Div_3/problem_D.cpp
+++ CodeForces/Rounds/Round_1084_Div_3/problem_D.cpp
@@ -14,8 +14,39 @@
 //===----------------------------------------------------------------------===//
 /* Main Solver Function */
 
-void solve() {
-  // Optimized solution here
+auto solve() -> void {
+  INT(n, x, y);
+  VecI32 p(n);
+  IN(p);
+
+  VecI32 outer;
+  outer.reserve(n - (y - x));
+  FOR(i, 0, x) outer.push_back(p[i]);
+  FOR(i, y, n) outer.push_back(p[i]);
+
+  I64 min_idx = x;
+  FOR(i, x + 1, y) {
+    if (p[i] < p[min_idx]) min_idx = i;
+  }
+  const I32 block_min = p[min_idx];
+
+  I64 insert_pos = sz(outer);
+  FOR(i, 0, sz(outer)) {
+    if (outer[i] > block_min) {
+      insert_pos = i;
+      break;
+    }
+  }
+
+  VecI32 answer;
+  answer.reserve(n);
+
+  FOR(i, 0, insert_pos) answer.push_back(outer[i]);
+  FOR(i, min_idx, y) answer.push_back(p[i]);
+  FOR(i, x, min_idx) answer.push_back(p[i]);
+  FOR(i, insert_pos, sz(outer)) answer.push_back(outer[i]);
+
+  OUT(answer);
 }
 
 //===----------------------------------------------------------------------===//
