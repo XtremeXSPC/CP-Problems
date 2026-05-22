d5fb3eb8eb977080f218b88ecfb5bf29f82a0184
LCS-Dev.Mac <xtremexspc@gmail.com>
2026-05-22 14:01:52 +0200

    refactor(templates): Drop redundant include guards and add Types marker.
    - Remove the parallel #ifndef CP_*_HPP_INCLUDED guards from Types.hpp, ScalarTypes.hpp, and ContainerAliases.hpp; #pragma once already covers every toolchain we target.
    - Add a stable "// END Types section" marker at the bottom of ContainerAliases.hpp so the flattener has a portable anchor for inserting using namespace std; after the type-system block.

---
+++ CodeForces/Algorithms/templates/ContainerAliases.hpp
@@ -0,0 +1,112 @@
+#pragma once
+#include "ScalarTypes.hpp"
+
+//===----------------------------------------------------------------------===//
+/* Container and Utility Aliases */
+
+using String     = std::string;
+using StringView = std::string_view;
+
+template <class T>
+using Vec = std::vector<T>;
+template <class T>
+using Deque = std::deque<T>;
+template <class T>
+using List = std::list<T>;
+template <class T, Size N>
+using Array = std::array<T, N>;
+template <Size N>
+using BitSet = std::bitset<N>;
+
+template <class T>
+using Set = std::set<T>;
+template <class T>
+using MultiSet = std::multiset<T>;
+template <class K, class V>
+using Map = std::map<K, V>;
+template <class K, class V>
+using MultiMap = std::multimap<K, V>;
+template <class T>
+using UnorderedSet = std::unordered_set<T>;
+template <class K, class V>
+using UnorderedMap = std::unordered_map<K, V>;
+
+template <class T, class Compare>
+using OrderedSetBy = std::set<T, Compare>;
+template <class T, class Compare>
+using OrderedMultiSetBy = std::multiset<T, Compare>;
+template <class K, class V, class Compare>
+using OrderedMapBy = std::map<K, V, Compare>;
+template <class K, class V, class Compare>
+using OrderedMultiMapBy = std::multimap<K, V, Compare>;
+template <class T, class Hash, class Eq = std::equal_to<T>>
+using HashedSetBy = std::unordered_set<T, Hash, Eq>;
+template <class K, class V, class Hash, class Eq = std::equal_to<K>>
+using HashedMapBy = std::unordered_map<K, V, Hash, Eq>;
+
+template <class T>
+using Stack = std::stack<T, std::deque<T>>;
+template <class T>
+using Queue = std::queue<T, std::deque<T>>;
+template <class T>
+using PriorityQueue = std::priority_queue<T, std::vector<T>>;
+template <class T>
+using MinPriorityQueue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+template <class T, class Container>
+using StackIn = std::stack<T, Container>;
+template <class T, class Container>
+using QueueIn = std::queue<T, Container>;
+template <class T, class Container, class Compare>
+using PriorityQueueBy = std::priority_queue<T, Container, Compare>;
+template <class T, class Container = std::vector<T>>
+using MinPriorityQueueIn = std::priority_queue<T, Container, std::greater<T>>;
+
+template <class T, class U>
+using Pair = std::pair<T, U>;
+template <class... Args>
+using Tuple = std::tuple<Args...>;
+template <class T>
+using Optional = std::optional<T>;
+template <class... Ts>
+using Variant = std::variant<Ts...>;
+template <class Signature>
+using Function = std::function<Signature>;
+template <class T>
+using Span = std::span<T>;
+
+template <class T>
+using Vec2D = Vec<Vec<T>>;
+template <class T>
+using Vec3D = Vec<Vec2D<T>>;
+template <class T>
+using Vec4D = Vec<Vec3D<T>>;
+
+template <class T>
+using Vec2 = Vec2D<T>;
+template <class T>
+using Vec3 = Vec3D<T>;
+template <class T>
+using Vec4 = Vec4D<T>;
+
+using VecI32  = Vec<I32>;
+using VecI64  = Vec<I64>;
+using VecBool = Vec<bool>;
+using VecStr  = Vec<String>;
+using VecU8   = Vec<U8>;
+using VecU16  = Vec<U16>;
+using VecU32  = Vec<U32>;
+using VecU64  = Vec<U64>;
+using VecF64  = Vec<F64>;
+using VecF80  = Vec<F80>;
+
+template <class T, class U>
+using VecPair = Vec<Pair<T, U>>;
+
+using PairI32    = Pair<I32, I32>;
+using PairI64    = Pair<I64, I64>;
+using PairF80    = Pair<F80, F80>;
+using VecPairI32 = Vec<PairI32>;
+using VecPairI64 = Vec<PairI64>;
+
+// END Types section
---
+++ CodeForces/Algorithms/templates/ScalarTypes.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef CP_TYPES_NO_PREAMBLE
+#include "Preamble.hpp"
+#endif
+
+//===----------------------------------------------------------------------===//
+/* Scalar Type Aliases */
+
+using I8  = std::int8_t;
+using I16 = std::int16_t;
+using I32 = std::int32_t;
+using I64 = std::int64_t;
+using U8  = std::uint8_t;
+using U16 = std::uint16_t;
+using U32 = std::uint32_t;
+using U64 = std::uint64_t;
+
+#if HAS_INT128
+using I128 = __int128;
+using U128 = unsigned __int128;
+#else
+using I128 = std::int64_t;
+using U128 = std::uint64_t;
+#endif
+
+using F32 = float;
+using F64 = double;
+using F80 = long double;
+
+#if HAS_FLOAT128
+using F128 = __float128;
+#else
+using F128 = long double;
+#endif
+
+template <typename T>
+using Limits = std::numeric_limits<T>;
+
+using Size = std::size_t;
+using Diff = std::ptrdiff_t;
+using Byte = std::byte;
